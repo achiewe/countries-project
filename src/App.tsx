ad660f237cfe4e4f43c8031ac7fd69b553c0751b
achiewe <mamukashviliachi706@gmail.com>
2024-02-03 17:55:50 +0400

    insert app component inside the QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import { QueryClientProvider, QueryClient } from "react-query";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <div> </div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>imean heroebi</div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
