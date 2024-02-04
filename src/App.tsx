f66a1b63e9ed99fb33dae7643c2034e5b4d4729c
achiewe <mamukashviliachi706@gmail.com>
2024-02-04 15:35:55 +0400

    remove app css from the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import "./App.css";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import Heroes from "./components/Heroes";
+import GlobalStyles from "./GlobalStyles";
 
 const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>imean heroebi</div>
-      <Heroes />
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      <main>
+        <GlobalStyles />
+        <div>imean heroebi</div>
+        <Heroes />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </main>
     </QueryClientProvider>
   );
 }
