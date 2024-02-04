847cb06c2a0d73d5e35e0e42cefa9a2104ae0637
achiewe <mamukashviliachi706@gmail.com>
2024-02-04 15:44:15 +0400

    create pages folder where add the file tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,28 @@ import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import Heroes from "./components/Heroes";
 import GlobalStyles from "./GlobalStyles";
+import styled from "styled-components";
 
 const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <main>
+      <MainContainer>
         <GlobalStyles />
-        <div>imean heroebi</div>
         <Heroes />
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-      </main>
+      </MainContainer>
     </QueryClientProvider>
   );
 }
 
+const MainContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f2f2f2;
+`;
 export default App;
---
+++ src/page/Airports.tsx
@@ -0,0 +1,3 @@
+export default function Airports() {
+  return <div>Airports</div>;
+}
