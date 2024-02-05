6669bf09204fc34aaa53cf802fd97ecb9f2df02e
achiewe <mamukashviliachi706@gmail.com>
2024-02-05 11:21:43 +0400

    add the comment on the porject

--- src/hooks/useSuperHeroesData.tsx
+++ src/hooks/useSuperHeroesData.tsx
@@ -1,21 +1,21 @@
-import { useQuery } from "react-query";
-import axios from "axios";
+// import { useQuery } from "react-query";
+// import axios from "axios";
 
-const fetchSuperheroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
-};
-export const useSuperHeroesData = (onSuccess: any, onError: any) => {
-  return useQuery(
-    "super-heroes",
-    fetchSuperheroes,
+// const fetchSuperheroes = () => {
+//   return axios.get("http://localhost:4000/superheroes");
+// };
+// export const useSuperHeroesData = (onSuccess: any, onError: any) => {
+//   return useQuery(
+//     "super-heroes",
+//     fetchSuperheroes,
 
-    {
-      onSuccess,
-      onError,
-      select: (data) => {
-        const superHeroNames = data.data.map((hero: any) => hero.name);
-        return superHeroNames;
-      },
-    }
-  );
-};
+//     {
+//       onSuccess,
+//       onError,
+//       select: (data) => {
+//         const superHeroNames = data.data.map((hero: any) => hero.name);
+//         return superHeroNames;
+//       },
+//     }
+//   );
+// };
