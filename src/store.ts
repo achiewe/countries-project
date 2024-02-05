6a893b7f55f6211a7bf76ee9899c390425ebd1e4
achiewe <mamukashviliachi706@gmail.com>
2024-02-05 18:56:17 +0400

    add the new state in the store

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,18 @@ import { create } from "zustand";
 
 type CountryStore = {
   country: string;
+  shortCountry: string;
   setCountry: (newCountry: string) => void;
+  setShortCountry: (newCoutry: string) => void;
 };
 
 export const useCountryStore = create<CountryStore>((set) => ({
   country: "",
+  shortCountry: "",
   setCountry: (newCountry: string) => {
     set({ country: newCountry });
+  },
+  setShortCountry: (newCountry: string) => {
+    set({ shortCountry: newCountry });
   },
 }));
