8099b6a9df22a8685a8defa2c62751d26218a984
achiewe <mamukashviliachi706@gmail.com>
2024-02-06 11:30:48 +0400

    style the header container

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,3 +1,15 @@
+import styled from "styled-components";
+
 export default function Header() {
-  return <div>Header</div>;
+  return <HeaderContainer>Header</HeaderContainer>;
 }
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  background-color: #efefef;
+  max-width: 700px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 20px;
+`;
