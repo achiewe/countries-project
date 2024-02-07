68cbf0de9aab905adaa6decfd0b2a257aea2db14
achiewe <mamukashviliachi706@gmail.com>
2024-02-07 14:44:05 +0400

    import component from the mui

--- src/components/currency/CurrencyConverter.tsx
+++ src/components/currency/CurrencyConverter.tsx
@@ -1,9 +1,26 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import styled from "styled-components";
 
 export default function CurrencyConverter() {
   return (
     <ConverterConatiner>
       <h1> Currency Exchange</h1>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value="12"
+          label="Age"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
     </ConverterConatiner>
   );
 }
