425fb815acdbd250a172ccb99d3cfc854d36f75f
achiewe <mamukashviliachi706@gmail.com>
2024-02-07 16:53:01 +0400

    create in the component folder Aiproprts folder where create file compoennt for the paren airports component

---
+++ src/components/Airports/AirportsList.tsx
@@ -0,0 +1,21 @@
+import { TextField } from "@mui/material";
+import styled from "styled-components";
+
+export default function AirportsList() {
+  return (
+    <AirportsContainer>
+      <h1> Airports</h1>
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <div className="currencyExchangeDiv"></div>
+    </AirportsContainer>
+  );
+}
+
+const AirportsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+`;
