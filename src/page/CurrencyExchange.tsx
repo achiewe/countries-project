13a8673814d19781a6a1c37f3ab37bef5c29d1e5
achiewe <mamukashviliachi706@gmail.com>
2024-02-07 12:00:39 +0400

    style currency container

--- src/page/CurrencyExchange.tsx
+++ src/page/CurrencyExchange.tsx
@@ -1,3 +1,13 @@
+import styled from "styled-components";
+
 export default function CurrencyExchange() {
-  return <div>Exchange</div>;
+  return <CurrencyContainer>Exchange</CurrencyContainer>;
 }
+
+const CurrencyContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
