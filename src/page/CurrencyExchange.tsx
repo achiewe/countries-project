77421e9fc424b706aec02b700cd15bd6004b2cd7
achiewe <mamukashviliachi706@gmail.com>
2024-02-07 12:09:33 +0400

    add the h1 tag in the main currency component

--- src/page/CurrencyExchange.tsx
+++ src/page/CurrencyExchange.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
+import CurrencyConverter from "../components/currency/CurrencyConverter";
 
 export default function CurrencyExchange() {
-  return <CurrencyContainer>Exchange</CurrencyContainer>;
+  return (
+    <CurrencyContainer>
+      <h1> Currency Exchange</h1>
+      <CurrencyConverter />
+    </CurrencyContainer>
+  );
 }
 
 const CurrencyContainer = styled.div`
@@ -9,5 +15,13 @@ const CurrencyContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 30px;
   justify-content: center;
+
+  h1 {
+    font-size: 18px;
+    font-weight: 800;
+    color: black;
+    font-style: normal;
+  }
 `;
