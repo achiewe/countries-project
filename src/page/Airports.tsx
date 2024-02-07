ca2965a7b7ce37bfb8e61724a8078da0b1283b56
achiewe <mamukashviliachi706@gmail.com>
2024-02-07 16:41:08 +0400

    add the Airports container in the component Airports and style it

--- src/page/Airports.tsx
+++ src/page/Airports.tsx
@@ -1,3 +1,18 @@
+import styled from "styled-components";
+
 export default function Airports() {
-  return <div>Airports</div>;
+  return <AirportsContainer></AirportsContainer>;
 }
+
+const AirportsContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  background-color: #efefef;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  border: 1px solid grey;
+  padding: 30px 0;
+`;
