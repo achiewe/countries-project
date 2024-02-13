0e7966f66f29a50b9d781becddace3904a49e0fc
achiewe <mamukashviliachi706@gmail.com>
2024-02-13 12:36:12 +0400

    import useNavigate to use it for when onchange the country name change navigate

--- src/components/Home/InputCountry.tsx
+++ src/components/Home/InputCountry.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useCountryStore } from "../../store";
 import CountryType from "../../../type";
+import { useNavigate } from "react-router-dom";
 
 // InputCountry function
 export default function InputCountry() {
