140923abe6cbf7b80e86e02998922185700264af
achiewe <mamukashviliachi706@gmail.com>
2024-02-15 22:15:11 +0400

    remove state from the component

--- src/components/Home/InputCountry.tsx
+++ src/components/Home/InputCountry.tsx
@@ -11,7 +11,6 @@ import CountryType from "../../../type";
 export default function InputCountry() {
   const setAllCountries = useCountryStore((state) => state.setAllCountries);
   const setCountryInfo = useCountryStore((state) => state.setCountryInfo);
-  const country = useCountryStore((state) => state.country);
   const [selectedCountry, setSelectedCountry] = useState<string>("");
 
   useEffect(() => {
