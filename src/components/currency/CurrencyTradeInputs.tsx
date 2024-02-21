c5640f6cb5e4cb40fead2be8dfbcc7e8d2a259af
achiewe <mamukashviliachi706@gmail.com>
2024-02-21 14:38:44 +0400

    style the COuntainerCurrencyInputs

--- src/components/currency/CurrencyTradeInputs.tsx
+++ src/components/currency/CurrencyTradeInputs.tsx
@@ -1,3 +1,13 @@
+import styled from "styled-components";
+
 export default function CurrencyTradeInputs() {
-  return <div>CurrencyExchange</div>;
+  return <ContainerCurrencyInputs></ContainerCurrencyInputs>;
 }
+
+const ContainerCurrencyInputs = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
