16f923111baa117b86d3e15d3ee415d829317f4d
achiewe <mamukashviliachi706@gmail.com>
2024-02-22 18:40:21 +0400

    remove function to fetch currency from the component

--- src/components/currency/FetchCurrency.ts
+++ src/components/currency/FetchCurrency.ts
@@ -8,34 +8,4 @@ export const fetchCurrencyInfo = async () => {
   const CurrencyQueryKey = ["cachedCurrency", countryInfo?.[0]?.name];
 
   // Fetching currency conversion rates
-  const { data: currency, isLoading: currencyLoading } = useQuery(
-    CurrencyQueryKey,
-    async () => {
-      try {
-        // Construct the URL with necessary parameters
-        const url = `https://api.exchangerate.host/convert?from=USD&to=GBP&amount=10&access_key=${currencyApiKey}`;
-
-        // Make GET request to fetch currency conversion rates
-        const response = await axios.get(url);
-
-        console.log("API Response:", response.data);
-
-        // Check if the response indicates success
-        if (response.data.success) {
-          console.log("Currency conversion result:", response.data.result);
-          return response.data.result;
-        } else {
-          // Log the error if the response indicates failure
-          console.error("Error fetching currency:", response.data.error);
-          throw new Error(response.data.error.info);
-        }
-      } catch (error) {
-        // Log and rethrow the error to be caught by react-query
-        console.error("Error fetching currency:", error);
-        throw error;
-      }
-    }
-  );
-
-  return currencyLoading;
 };
