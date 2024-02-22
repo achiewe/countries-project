88d41cb56012c56bf65cd6887c4e3cb54d6c8789
achiewe <mamukashviliachi706@gmail.com>
2024-02-22 21:38:04 +0400

    delete FetchCurrency component

--- src/components/currency/FetchCurrency.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "react-query";
-import { useCountryStore } from "../../store";
-import axios from "axios";
-
-export const fetchCurrencyInfo = async () => {
-  const countryInfo = useCountryStore((state) => state.countryInfo);
-
-  // Fetching currency conversion rates
-};
