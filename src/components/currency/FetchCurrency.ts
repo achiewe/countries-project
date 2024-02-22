67dd675dc6fdda3dad0d1e3e60e62e35ed82f1aa
achiewe <mamukashviliachi706@gmail.com>
2024-02-22 13:47:04 +0400

    add the function for bring the data

--- src/components/currency/FetchCurrency.ts
+++ src/components/currency/FetchCurrency.ts
@@ -4,22 +4,22 @@ import axios from "axios";
 const currencyApiKey = import.meta.env.VITE_REACT_APP_EXCHANGE_API_KEY;
 
 export const fetchCurrencyInfo = async () => {
-  // const countryInfo = useCountryStore((state) => state.countryInfo);
-  // const CurrencyQueryKey = ["cachedCurrency", countryInfo?.[0]?.name];
-  // const { data: currency, isLoading: currencyLoading } = useQuery(
-  //   CurrencyQueryKey,
-  //   async () => {
-  //     try {
-  //       const response = await axios.get(
-  //         "http://localhost:3000/api/convert?from=USD&to=GBP",
-  //         {
-  //           headers: { "X-Api-Key": currencyApiKey },
-  //         }
-  //       );
-  //       console.log(response.data, "me var ak usd sds");
-  //       return response.data.result;
-  //     } catch (error) {}
-  //   }
-  // );
-  // return currencyLoading;
+  const countryInfo = useCountryStore((state) => state.countryInfo);
+  const CurrencyQueryKey = ["cachedCurrency", countryInfo?.[0]?.name];
+  const { data: currency, isLoading: currencyLoading } = useQuery(
+    CurrencyQueryKey,
+    async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/convert?from=USD&to=GBP",
+          {
+            headers: { "X-Api-Key": currencyApiKey },
+          }
+        );
+        console.log(response.data, "me var ak usd sds");
+        return response.data.result;
+      } catch (error) {}
+    }
+  );
+  return currencyLoading;
 };
