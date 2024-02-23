723cd40c79ea283e82d1c3a4c8fc06003b9acbe3
achiewe <mamukashviliachi706@gmail.com>
2024-02-23 20:19:40 +0400

    add the euro MenuItem

--- src/components/currency/CurrencyConverter.tsx
+++ src/components/currency/CurrencyConverter.tsx
@@ -18,6 +18,7 @@ export default function CurrencyConverter() {
             <MenuItem>₾</MenuItem>
             <MenuItem>$</MenuItem>
             <MenuItem>£</MenuItem>
+            <MenuItem>€</MenuItem>
           </Select>
         </FormControl>
 
